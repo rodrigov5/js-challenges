ea4b9e9fca49eb582ae48d25f2723603fb467b86
Rodrigo Veloso <57206601+rodrigov5@users.noreply.github.com>
2024-07-15 21:03:45 -0300

    intersection

---
+++ 02-basic/02-array-intersection/array-intersection-run.js
@@ -0,0 +1,8 @@
+const arraIntesection = require("./array-intersection")
+
+let arr1 = [1,2,3,4,5,6];
+let arr2 = [1,2,3,8,7,9, 10, 11];
+
+const result = arraIntesection(arr1, arr2);
+
+console.log(result);
